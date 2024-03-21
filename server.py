2779dc827848cb05655b0d1c16ed8238152dbea5
Noriaki528 <130439620+Noriaki528@users.noreply.github.com>
2024-03-21 10:12:51 +0900

    改善

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def update_lux():
    lux = request.form["lux"]
    try:
         f = open(file_path, 'a')
-        f.write(time + "," + lux )
+        f.write(time + "," + lux + "," +"\n")
         return "succeeded to write" 
    except Exception as e:
      print(e)
